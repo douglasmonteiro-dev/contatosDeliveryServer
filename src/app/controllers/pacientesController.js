39941602d1e3aed61687c39399027282d8314a1f
Douglas Monteiro <douglasmonteiro@icloud.com>
2019-02-11 10:10:27 -0200

    correcao de bug

--- src/app/controllers/pacientesController.js
+++ src/app/controllers/pacientesController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authMiddleware = require('../middlewares/auth');
-const User = require('../models/User');
+const User = require('../models/user');
 
 
 const router = express.Router();
