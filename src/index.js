1ec64ba5d9ebb20bc5f79fd2e2e9e09a3a2ef267
Douglas Monteiro <douglasmonteiro@icloud.com>
2019-08-06 16:36:23 -0300

    fix allow origin

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost:4200");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD");
     next();
